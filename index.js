c4e88f854b859b2e682dc98856db0e7fb4da4b34
Mattia Fortunato <matfortu@gmail.com>
2019-06-19 15:56:11 +0200

    riscritto l'html utilizzando bootstrap

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var utenti = ['anon'];
+var utenti = [];
 
 
 app.use('/static', express.static(__dirname +'/static'));
@@ -13,36 +13,38 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket) {
 	console.log('un utente si è connesso');
-	socket.username = "anon";
+	//socket.username = "anon";
 
-	socket.on('cambio_username', (data) => {
-		if(data.username != null){
+	socket.on('set_username', (data) => {
+		socket.username = data.username;
+		console.log('nuovo username: ' + socket.username);
+		utenti.push(socket.username);
+		io.emit('messaggio_server', socket.username + ' si è unito/a alla chat!');
+		console.log(utenti);
+	});
+
+	socket.on('nome_libero', (data) => {
+		if(data.username != ""){
 			if(utenti.indexOf(data.username) == -1) {
-				socket.username = data.username;
-				console.log('nuovo username: ' + socket.username);
-				utenti.push(socket.username);
-				io.emit('messaggio_server', socket.username + ' si è unito/a alla chat!');
+				console.log(utenti.indexOf(data.username));
+				socket.emit('libero');
 			}
-			else socket.emit('username_occupato');
 		}
+		else socket.emit('occupato');
 	});
 
 	socket.on('disconnect', function(){
-		console.log(socket.username + ' si è disconnesso');
-		io.emit('messaggio_server', socket.username + ' si è disconnesso');
-		var pos = utenti.indexOf(socket.username);
-		var rimosso = utenti.splice(pos, 1);
+		if(socket.username != undefined) {
+			console.log(socket.username + ' si è disconnesso');
+			io.emit('messaggio_server', socket.username + ' si è disconnesso');
+			var pos = utenti.indexOf(socket.username);
+			var rimosso = utenti.splice(pos, 1);
+		}
 	});
 
 	socket.on('messaggio', function(msg) {
-		if(socket.username != 'anon' && socket.username != null) {
-			console.log(socket.username + ' : ' + msg);
-			io.emit('messaggio', socket.username + ' : ' + msg);
-		}
-		else {
-			console.log('un utente anonimo sta provando a scrivere in chat');
-			socket.emit('anonimo');
-		}
+		console.log(socket.username + ' : ' + msg);
+		io.emit('messaggio', socket.username + ' : ' + msg);
 	});
 });
 
